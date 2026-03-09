e60a78b76aa5eef2be45b02f171fbe57b868582c
DalilaManu <dmoliveira1205@gmail.com>
2026-03-09 13:30:57 +0100

    feat: Añadir posicion(enum)

---
+++ src/posicion.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Posicion(Enum):
+    CENTER = "Center"
+    LEFTWING = "Left Wing"
+    RIGHTWING = "Right Wing"
+    LEFT_DEFENSEMAN = "Left Defenseman"
+    RIGHT_DEFENSEMAN = "Right Defenseman"
+    GOALIE = "Goalie"
